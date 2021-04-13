9f49a79275cd774720c97e4aa2842dc1117eb414
User <hopper0228@gmail.com>
2021-04-13 15:33:30 +0800

    11

---
+++ src/main/java/com/tom/racing/Racing.java
@@ -0,0 +1,10 @@
+package com.tom.racing;
+
+public class Racing {
+    public static void main(String[] args){
+        Horse h1 = new Horse();
+        Horse h2 = new Horse();
+        h1.start();
+        h2.start();
+    }
+}
---
+++ src/main/java/com/tom/rpg/Solider.java
@@ -0,0 +1,14 @@
+package com.tom.rpg;
+
+public class Solider extends Villager {
+    public Solider(int x, int y) {
+        super(x, y);
+        hp = 200;
+    }
+
+    @Override
+    public void walk() {
+
+
+    }
+}

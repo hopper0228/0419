be36810069cfd9eee6248f80a3465f9ebf709e58
lail26266@gmail.com <lail26266@gmail.com>
2021-04-19 21:17:50 +0800

    0419

--- src/main/java/com/tom/racing/Horse.java
+++ src/main/java/com/tom/racing/Horse.java
@@ -3,7 +3,7 @@ package com.tom.racing;
 public class Horse extends Thread{
     public  void run(){
         for(int i = 0 ; i<1000; i++){
-            System.out.println(getName() + i);
+            System.out.println(getName() + " " + i);
         }
     }
 }
--- src/main/java/com/tom/racing/Racing.java
+++ src/main/java/com/tom/racing/Racing.java
@@ -4,6 +4,9 @@ public class Racing {
     public static void main(String[] args){
         Horse h1 = new Horse();
         Horse h2 = new Horse();
+        Runnable h3 = new Runnable();
+        Thread thread = new Thread(h3);
+        thread.start();
         h1.start();
         h2.start();
     }

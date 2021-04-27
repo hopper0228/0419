1416aaa822517f1091f4a16310cb85b76523e08d
hopper <hopper0228@gmail.com>
2021-04-27 16:26:43 +0800

    0427

--- src/main/java/com/tom/test/test.java
+++ src/main/java/com/tom/test/test.java
@@ -2,14 +2,23 @@ package com.tom.test;
 
 public class test {
     public static void main(String[] args) {
-        Box[] boxes = {new Box1(), new Box2(),new Box3(),new Box4(),new Box5()};
+        Box[] boxes = new Box[5];
+        boxes[0] = new Box1();
+        boxes[1] = new Box2();
+        boxes[2] = new Box3();
+        boxes[3] = new Box4();
+        boxes[4] = new Box5();
+        System.out.println(boxes[3].price);
+        //Box[] boxes = {new Box1(), new Box2(),new Box3(),new Box4(),new Box5()};
         int length = 20;
-        int width = 10;
-        int height = 10;
+        int width = 5;
+        int height = 5;
         for (Box box : boxes) {
+//            Box1 box1 = (Box1) box;
             if(box.vaildate(length,width,height))
             {
                 System.out.println(box.name + " is " + box.price);
+                break;
             }
         }
 
